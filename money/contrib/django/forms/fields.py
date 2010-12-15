b31ccc1b82d0f1b8efd1548405076f3d1a1a7b83
Paul Oswald <pauloswald@gmail.com>
2010-12-15 16:21:16 +0900

    Pass kwargs (like localize=True) into the Decimal field

--- money/contrib/django/forms/fields.py
+++ money/contrib/django/forms/fields.py
@@ -5,14 +5,17 @@ from money import Money, CURRENCY
 
 class MoneyField(forms.MultiValueField):
     def __init__(self, choices=None, decimal_places=2, max_digits=12, *args, **kwargs):
+        # Note that we catch args and kwargs that must only go to one field
+        # or the other. The rest of them pass onto the decimal field.
         choices = choices or list(( (u"%s" % (c.code,), u"%s - %s" % (c.code, c.name)) for i, c in sorted(CURRENCY.items()) if c.code != 'XXX'))
 
         self.widget = CurrencySelectWidget(choices)
 
         fields = (
-            forms.DecimalField(
+            forms.DecimalField(*args,
                 decimal_places=decimal_places,
-                max_digits=max_digits),
+                max_digits=max_digits,
+                **kwargs),
             forms.ChoiceField(choices=choices)
         )
         super(MoneyField, self).__init__(fields, *args, **kwargs)
